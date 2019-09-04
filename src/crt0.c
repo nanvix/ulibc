2bbfcd90062fadb994b0d902e54407d28543f3dc
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-09-04 14:01:20 +0200

    Feature: Adding crt0

---
+++ src/crt0.c
@@ -0,0 +1,56 @@
+/*
+ * MIT License
+ *
+ * Copyright(c) 2011-2019 The Maintainers of Nanvix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <ulibc/glue.h>
+#include <ulibc/stdio.h>
+#include <stddef.h>
+
+/*
+ * Main routine.
+ */
+extern int main(int argc, const char *argv[]);
+
+/**
+ * @brief Environment variables.
+ */
+char **environ = NULL;
+
+/*
+ * Entry point of the program.
+ */
+void ___start(int argc, const char *argv[], char **envp)
+{
+	int ret;
+	static char stdout_buffer[BUFSIZ];
+	static char stderr_buffer[BUFSIZ];
+
+	setvbuf(stdout, stdout_buffer, _IOLBF, BUFSIZ);
+	setvbuf(stdout, stderr_buffer, _IOLBF, BUFSIZ);
+
+	environ = envp;
+
+	ret = main(argc, argv);
+
+	___exit(ret);
+}
